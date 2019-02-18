46198f6d441ee9a27b615be9be369a0b4601dff7
swirlflag <swirlflag>
2019-02-18 10:07:49 +0900

    dist test

---
+++ portfolio/src/routes/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
+
+
+export const router = new VueRouter({
+
+  mode: 'history',
+
+  routes: [
+
+    {
+      path: '/',
+      redirect: '/index',
+    },
+
+  ],
+
+});
